e8d51d30733256d9f6b0527e319d3da4327b0fbc
Ethan Brittain <70303464+ekbritt99@users.noreply.github.com>
2023-09-14 17:26:15 -0400

    Integrate uploadthing for server creation
    User can upload image to use when creating a server

--- discord-clone/components/models/initial-model.tsx
+++ discord-clone/components/models/initial-model.tsx
@@ -3,6 +3,7 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 
 import {
     Dialog,
@@ -24,6 +25,7 @@ import {
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -39,6 +41,12 @@ const formSchema = z.object({
 
 export const InitialModel = () => {
 
+    const [isMounted, setIsMounted] = useState(false);
+    
+    useEffect (() => {
+        setIsMounted(true);
+    }, []);
+
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -51,6 +59,10 @@ export const InitialModel = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values);
+    }
+
+    if (!isMounted) {
+        return null;
     }
 
     return (
@@ -69,7 +81,21 @@ export const InitialModel = () => {
                     className="space-y-8">
                         <div className="space-y-8 px-6">
                         <div className="flex items-center justify-center text-center">
-                            TODO: Image Upload
+                            <FormField 
+                                control={form.control}
+                                name="imageUrl"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <FileUpload 
+                                                endpoint="serverImage"
+                                                value={field.value}
+                                                onChange={field.onChange}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
                         </div>
 
                         <FormField 
@@ -93,12 +119,13 @@ export const InitialModel = () => {
                                             {...field}
                                         />
                                     </FormControl>
+                                    <FormMessage />
                                 </FormItem>
                             )}
                         />
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
-                            <Button disabled={isLoading}>
+                            <Button variant="primary" disabled={isLoading}>
                                 Create
                             </Button>
                         </DialogFooter>
