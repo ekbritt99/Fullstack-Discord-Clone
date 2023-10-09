2ff56bf9558e93f40c3ca25fe3c0967be5e60be5
Ethan Brittain <70303464+ekbritt99@users.noreply.github.com>
2023-10-09 17:36:04 -0400

    Server sidebar displays all channels and members
    Channels are separated by type and can be quickly created from their spot in the sidebar.

--- discord-clone/components/server/server-search.tsx
+++ discord-clone/components/server/server-search.tsx
@@ -4,6 +4,7 @@ import { Search } from "lucide-react";
 import React, { Fragment, useEffect, useState } from "react";
 import { StringLiteral } from "typescript";
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerSearchProps {
     data: {
@@ -22,6 +23,8 @@ export const ServerSearch = ({
     data
 }: ServerSearchProps) => {
     const [open, setOpen] = useState(false);
+    const router = useRouter();
+    const params = useParams();
 
     useEffect(() => {
         const down = (e: KeyboardEvent) => {
@@ -35,6 +38,18 @@ export const ServerSearch = ({
         document.addEventListener("keydown", down);
         return () => document.removeEventListener("keydown", down)
     }, []);
+
+    const onClick = ({ id, type }: { id:string, type: "channel" | "member" }) => {
+        setOpen(false);
+
+        if (type === "member") {
+            return router.push(`/servers/${params?.serverId}/converations/${id}`)
+        }
+        
+        if (type === "channel") {
+            return router.push(`/servers/${params?.serverId}/channels/${id}`)
+        }
+    }
 
     return ( 
         <>
@@ -74,7 +89,7 @@ export const ServerSearch = ({
                             <CommandGroup key={label} heading={label}>
                                 {data?.map(({ id, icon, name }) => {
                                     return (
-                                        <CommandItem key={id}>
+                                        <CommandItem key={id} onSelect={() => onClick({ id, type })}>
                                             {icon}
                                             <span>{name}</span>
                                         </CommandItem>
